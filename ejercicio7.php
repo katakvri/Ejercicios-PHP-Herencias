1e6b2c4d3381de8d601e81a2c316a4ec58efe3c5
mverikas <68635436+katakvri@users.noreply.github.com>
2021-06-22 17:25:58 +0200

    primeras funciones ejercicio 7

--- ejercicio7.php
+++ ejercicio7.php
@@ -10,11 +10,70 @@
     <?php
     class Electrodomestico
     {
-        protected $precioBase = "100";
-        protected $color = "blanco";
-        protected $consumo = "F";
-        protected $peso = "5";
+        protected $precio;
+        protected $color;
+        protected $consumo;
+        protected $peso;
 
+        public function __construct($precio = "100", $peso = "5", $cons = "F", $col = "blanco") 
+        {
+            $this->precio = $precio;
+            $this->color = $col;
+            $this->consumo = $cons;
+            $this->peso = $peso;
+        }
+
+        public function comprobarConsumoEnergetico($letra) 
+        {
+            switch ($letra) {
+                case 'A':
+                    echo "su consumo es de letra A lo que esta valorada en 100";
+                    break;
+                case 'B':
+                    echo "su consumo es de letra B lo que esta valorada en 80";
+                    break;
+                case 'C':
+                    echo "su consumo es de letra C lo que esta valorada en 60";
+                    break;
+                case 'D':
+                    echo "su consumo es de letra D lo que esta valorada en 50";
+                    break;
+                case 'E':
+                    echo "su consumo es de letra E lo que esta valorada en 30";
+                    break;
+                case 'F':
+                    echo "su consumo es de letra F lo que esta valorada en 10";
+                    break;
+                default:
+                    echo "No se ha indicado correctamente la letra del consumo energetico";
+                    break;
+            }
+        }
+        
+        public function comprobarColor($color) 
+        {
+            
+            switch (strtolower($color)) {
+                case 'blanco':
+                    echo "Esta diponible el color Blanco";
+                    break;
+                case 'negro':
+                    echo "Esta diponible el color Negro";
+                    break;
+                case 'gris':
+                    echo "Esta diponible el color Gris";
+                    break;
+                case 'rojo':
+                    echo "Esta diponible el color Rojo";
+                    break;
+                case 'azul':
+                    echo "Esta diponible el color Azul";
+                    break;
+                default:
+                    echo "Este color no existe en el inventario";
+                    break;
+            }
+        }
     }
     
     ?>
